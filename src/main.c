c0d38c788d8e87f891dce2eda769d283563a7532
Quark1111 <tevrukovila@gmail.com>
2026-03-18 01:06:07 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,30 +5,26 @@
 int main(int argc, char* argv[])
 {
     if (argc == 3) {
-        Table* table = Createtable(argv[1]);
-        if (table) {
-            DrawTable(table, argv[2]);
-            DeleteTable(table);
+        Table* table = createTable(argv[1]);
+        if (table != NULL) {
+            drawTable(table, argv[2]);
+            deleteTable(table);
         }
-    } else if (argc == 1) {
-        char inputfile[256], outputfile[256];
+        return 0;
+    }
+
+    if (argc == 1) {
+        char input[256], output[256];
         printf("Enter input file: ");
-        if (scanf("%255s", inputfile) != 1) {
-            return 1;
-        }
+        scanf("s", input);
         printf("Enter output file: ");
-        if (scanf("%255s", outputfile) != 1) {
-            return 1;
+        scanf("%255s", output);
+        Table* table = createTable(input);
+        if (table != NULL) {
+            drawTable(table, output);
+            deleteTable(table);
         }
-        Table* table = Createtable(inputfile);
-        if (table) {
-            DrawTable(table, outputfile);
-            DeleteTable(table);
-        }
-    } else {
-        printf("Usage: %s [input.csv output.txt]\n", argv[0]);
-        printf("   or: %s (interactive mode)\n", argv[0]);
-        return 1;
+        return 0;
     }
     return 0;
 }
