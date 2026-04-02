c5af62ceaf49b278848da3d807aab981da6ea5e3
Ilya Tevrukov <tevrukovila@gmail.com>
2026-04-02 02:58:30 +0000

    Add states program: graph, heap, readfile, tests

---
+++ graph/src/heap.c
@@ -0,0 +1,106 @@
+#include "heap.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static void siftUp(Heap* heap, unsigned int index)
+{
+    if (heap == NULL || heap->arrayNode == NULL) {
+        return;
+    }
+    while (index > 0) {
+        unsigned int parent = (index - 1) / 2;
+        if (heap->arrayNode[parent].dist > heap->arrayNode[index].dist) {
+            Node temp = heap->arrayNode[parent];
+            heap->arrayNode[parent] = heap->arrayNode[index];
+            heap->arrayNode[index] = temp;
+            index = parent;
+        } else {
+            break;
+        }
+    }
+}
+
+static void siftDown(Heap* heap, unsigned int index)
+{
+    if (heap == NULL || heap->arrayNode == NULL) {
+        return;
+    }
+    while (index*2 + 1 < heap->len) {
+        unsigned int indexChild = index*2 + 1;
+        if (indexChild + 1 < heap->len && heap->arrayNode[indexChild].dist > heap->arrayNode[indexChild + 1].dist) {
+            indexChild += 1;    
+        }
+        if (heap->arrayNode[indexChild].dist < heap->arrayNode[index].dist) {
+            Node temp = heap->arrayNode[indexChild];
+            heap->arrayNode[indexChild] = heap->arrayNode[index];
+            heap->arrayNode[index] = temp;
+            index = indexChild;
+        } else {
+            break;
+        }
+    }
+}
+
+Heap* createHeap()
+{
+    Heap* heap = malloc(sizeof(Heap));
+    if (heap == NULL) {
+        return NULL;
+    }
+    heap->len = 0;
+    heap->capacity = 1;
+    heap->arrayNode = calloc(1, sizeof(Node));
+    if (heap->arrayNode == NULL) {
+        free(heap);
+        return NULL;
+    }
+    return heap;
+}
+
+void insertHeap(Heap* heap, unsigned int city, int dist)
+{
+    if (heap == NULL || heap->arrayNode == NULL) {
+        return;
+    }
+    Node newNode;
+    newNode.city = city;
+    newNode.dist = dist;
+    if (heap->len == heap->capacity) {
+        Node* newArrayNode = realloc(heap->arrayNode, sizeof(Node) * heap->capacity * 2);
+        if (newArrayNode == NULL) {
+            return;
+        }
+        heap->capacity *= 2;
+        heap->arrayNode = newArrayNode;
+    }
+    heap->arrayNode[heap->len] = newNode;
+    heap->len++;
+    siftUp(heap, heap->len - 1);
+}
+void extractMinHeap(Heap* heap)
+{
+    if (heap == NULL || heap->arrayNode == NULL || heap->len == 0) {
+        return;
+    }
+    heap->arrayNode[0] = heap->arrayNode[heap->len - 1];
+    heap->len--;
+    siftDown(heap, 0);
+}
+
+Node* getMin(Heap* heap)
+{
+    if (heap == NULL || heap->arrayNode == NULL || heap->len == 0) {
+        return NULL;
+    }
+    return &(heap->arrayNode[0]);
+}
+
+void deleteHeap(Heap* heap)
+{
+    if (heap == NULL || heap->arrayNode == NULL) {
+        free(heap);
+        return;
+    }
+    free(heap->arrayNode);
+    free(heap);
+}
---
+++ graph/src/readfile.c
@@ -0,0 +1,108 @@
+#include "readfile.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static bool correctInputCities(unsigned int city, unsigned int size)
+{
+    return city <= size && city > 0;
+}
+
+Graph* readfile(unsigned int** capitals, unsigned int* len, char* inputfile)
+{
+    FILE* file = fopen(inputfile, "r");
+    
+    if (file == NULL) {
+        return NULL;
+    }
+    
+    unsigned int n, m;
+    if (fscanf(file, "%u %u", &n, &m) != 2) {
+        printf("incorrect input data format");
+        fclose(file);
+        return NULL;
+    }
+
+    unsigned int* cntVertex = calloc(n, sizeof(unsigned int));
+    if (cntVertex == NULL) {
+        fclose(file);  
+        return NULL;
+    }
+    
+    for (unsigned int i = 0; i < m; i++) {
+        unsigned city1, city2;
+        int len;
+        if(fscanf(file, "%u %u %d", &city1, &city2, &len) != 3 
+           || correctInputCities(city1, n) == 0 
+           || correctInputCities(city2, n) == 0) {
+            printf("incorrect input data format");
+            free(cntVertex);
+            fclose(file);
+            return NULL;
+        }
+        cntVertex[city1 - 1]++;
+        cntVertex[city2 - 1]++;
+    }
+    
+    rewind(file);
+    fscanf(file, "%u %u", &n, &m);
+    Graph* graph = createGraph(n);
+    if (graph == NULL) {
+        free(cntVertex);
+        fclose(file);
+        return NULL;
+    }
+    
+    for (unsigned int i = 0; i < n; i++) {
+        graph->vertex[i] = createVertex(cntVertex[i], i);
+        if (graph->vertex[i] == NULL) {
+            free(cntVertex);
+            deleteGraph(graph);
+            fclose(file);
+            return NULL;
+        }
+    }
+
+    for (unsigned int i = 0; i < m; i++) {
+        unsigned city1, city2;
+        int len;
+        fscanf(file, "%u %u %d", &city1, &city2, &len);
+        connectVertex(graph, city1, city2, len);
+    }
+
+    unsigned int cntStates;
+    if (fscanf(file, "%u", &cntStates) != 1) {
+        printf("incorrect input data format");
+        free(cntVertex);
+        deleteGraph(graph);
+        fclose(file);
+        return NULL;
+    }
+
+    unsigned int* caps = malloc(cntStates * sizeof(unsigned int));
+    if (caps == NULL) {
+        free(cntVertex);
+        deleteGraph(graph);
+        fclose(file);
+        return NULL;
+    }
+
+    for (unsigned int i = 0; i < cntStates; i++) {
+        unsigned int capital;
+        if (fscanf(file, "%u", &capital) != 1 || !correctInputCities(capital, n)) {
+            printf("incorrect input data format\n");
+            free(caps);
+            free(cntVertex);
+            deleteGraph(graph);
+            fclose(file);
+            return NULL;
+        }
+        caps[i] = capital;
+    }
+    free(cntVertex);
+    fclose(file);
+    
+    *capitals = caps;
+    *len = cntStates;
+    return graph;
+}
