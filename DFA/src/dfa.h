8ac2ed873bbeca29d18d2975ad7f7533be1fb347
Quark1111 <tevrukovila@gmail.com>
2026-04-12 04:14:27 +0300

    Update dfa.h

--- DFA/src/dfa.h
+++ DFA/src/dfa.h
@@ -7,23 +7,25 @@ typedef enum {
     emptyDfa
 } Result;
 
-typedef struct { 
-    unsigned int from; 
+typedef struct {
+    unsigned int from;
     char fromSymbol;
-    char toSymbol; 
-    unsigned int to; 
+    char toSymbol;
+    unsigned int to;
 } Transition;
 
 typedef struct {
-    Transition* transition;
+    Transition *transition;
     unsigned int countTransition;
     unsigned int countAccepting;
-    unsigned int* accepting;
+    unsigned int *accepting;
     unsigned int start;
 } Dfa;
 
-Dfa* createDfa(Transition* transition, unsigned int* accepting, 
-               unsigned int countTransition, unsigned int countAccepting, 
+Dfa *createDfa(Transition *transition, unsigned int *accepting,
+               unsigned int countTransition, unsigned int countAccepting,
                unsigned int start);
-void deleteDfa(Dfa* dfa);
-Result runDfa(Dfa* dfa, char* str);
+
+void deleteDfa(Dfa *dfa);
+
+Result runDfa(Dfa *dfa, char *str);
