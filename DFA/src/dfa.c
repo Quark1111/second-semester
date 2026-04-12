3ffec90ed3be7fb3074c8ab2cf1f951f3ec80002
Quark1111 <tevrukovila@gmail.com>
2026-04-12 04:20:34 +0300

    Update dfa.c

--- DFA/src/dfa.c
+++ DFA/src/dfa.c
@@ -1,11 +1,8 @@
-#include "dfa.h"
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 
-Dfa* createDfa(Transition* transition, unsigned int* accepting, 
-               unsigned int countTransition, unsigned int countAccepting, 
-               unsigned int start)
+Dfa* createDfa(Transition* transition, unsigned int* accepting, unsigned int countTransition, unsigned int countAccepting, unsigned int start)
 {
     if (transition == NULL || accepting == NULL) {
         return NULL;
@@ -13,10 +10,12 @@ Dfa* createDfa(Transition* transition, unsigned int* accepting,
     if (countTransition == 0 || countAccepting == 0) {
         return NULL;
     }
+
     Dfa* dfa = malloc(sizeof(Dfa));
     if (dfa == NULL) {
         return NULL;
     }
+
     dfa->transition = malloc(sizeof(Transition) * countTransition);
     if (dfa->transition == NULL) {
         free(dfa);
@@ -24,6 +23,7 @@ Dfa* createDfa(Transition* transition, unsigned int* accepting,
     }
     memcpy(dfa->transition, transition, sizeof(Transition) * countTransition);
     dfa->countTransition = countTransition;
+
     dfa->accepting = malloc(sizeof(unsigned int) * countAccepting);
     if (dfa->accepting == NULL) {
         free(dfa->transition);
@@ -32,6 +32,7 @@ Dfa* createDfa(Transition* transition, unsigned int* accepting,
     }
     memcpy(dfa->accepting, accepting, sizeof(unsigned int) * countAccepting);
     dfa->countAccepting = countAccepting;
+
     dfa->start = start;
     return dfa;
 }
@@ -43,32 +44,38 @@ static bool isAlphabet(Transition transition, char symbol)
 
 static Result move(Dfa* dfa, unsigned int* state, char symbol)
 {
-    bool UNKNOWN = 1;
+    bool unknown = 1;
+
     for (unsigned int i = 0; i < dfa->countTransition; i++) {
-        if (dfa->transition[i].from == *state && isAlphabet(dfa->transition[i], symbol)) {
+        if (dfa->transition[i].from == *state
+            && isAlphabet(dfa->transition[i], symbol)) {
             *state = dfa->transition[i].to;
             return accept;
         }
         if (isAlphabet(dfa->transition[i], symbol)) {
-            UNKNOWN = 0;
+            unknown = 0;
         }
     }
-    if (UNKNOWN) {
+
+    if (unknown) {
         return unknownSYMBOL;
     }
     return noTRANSITION;
 }
 
-Result runDfa(Dfa* dfa, char* str) {
+Result runDfa(Dfa* dfa, char* str)
+{
     if (dfa == NULL || dfa->accepting == NULL || dfa->transition == NULL) {
         return nullpointerexception;
     }
     if (dfa->countAccepting == 0 || dfa->countTransition == 0) {
         return emptyDfa;
     }
+
     unsigned int state = dfa->start;
     size_t len = strlen(str);
-    for (unsigned int i = 0; i < len; i++) {
+
+    for (size_t i = 0; i < len; i++) {
         Result flag = move(dfa, &state, str[i]);
         if (flag != accept) {
             return flag;
